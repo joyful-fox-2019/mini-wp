dc3c7a332f422ae813d95b4c82ce2174d347f6a2
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-08 22:28:32 +0700

    models done

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ if (process.env.NODE_ENV === 'development') require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('morgan');
+const cors = require('cors');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,10 +12,10 @@ app.use(morgan('dev'));
 app.use(cors());
 
 mongoose
-	.connect(process.env.MONGODB_URL)
+	.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('mongodb connected'))
 	.catch(err => console.log('mongodb connection failed', err));
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => 'app listening to port', PORT);
+app.listen(PORT, () => console.log('app listening to port', PORT));
--- server/models/Article.js
+++ server/models/Article.js
@@ -1,6 +1,33 @@
 import { Schema, model } from 'mongoose';
 
-const articleSchema = new Schema({});
+const articleSchema = new Schema({
+	title: {
+		type: String,
+		required: [true, 'Title required']
+	},
+	content: {
+		type: String
+	},
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	status: {
+		type: String,
+		enum: ['draft, posted, deleted'],
+		default: 'draft'
+	},
+	created_at: {
+		type: Date,
+		required: true,
+		default: new Date()
+	},
+	updated_at: {
+		type: Date,
+		required: true,
+		default: new Date()
+	}
+});
 
 const Article = model('Article', articleSchema);
 
