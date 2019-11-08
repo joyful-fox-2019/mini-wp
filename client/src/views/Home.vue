5981b09694f433d10192971448c04d7de45db078
dipadana <putudipadana@gmail.com>
2019-11-08 21:37:38 +0700

    Add footer

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,79 +1,155 @@
 <template>
-  <b-container style="margin-top: 3vh;">
 
-    <b-row class="no-gutters">
-      <b-col cols="12" md="6" class="">
-        <div class="big-image p-0 m-1">
-          <img class="imageBig" src="https://miro.medium.com/max/12000/1*1gByVzAtW0myl9e9CsGqhw.jpeg" width="100%">
-          <div class="centered">
-            <h3 class="m-0">For more information visit website</h3>
-            <small class="m-0">Dipadana, 20 January 2019</small>
+  <div>
+    <b-container style="margin-top: 3vh;">
+
+      <b-row class="no-gutters">
+        <b-col cols="12" md="6" class="">
+          <div class="big-image p-0 m-1">
+            <img class="imageBig" src="https://miro.medium.com/max/12000/1*1gByVzAtW0myl9e9CsGqhw.jpeg" width="100%">
+            <div class="centered">
+              <h3 class="m-0">For more information visit website</h3>
+              <small class="m-0">Dipadana, 20 January 2019</small>
+            </div>
+          </b-jumbot>
+        </b-col>
+
+        <b-col cols="12" md="6" class="">
+          <div class="big-image p-0 m-1">
+            <img class="imageBig" src="https://content-static.upwork.com/blog/uploads/sites/3/2019/06/11041941/11-Tips-to-Optimize-JavaScript-And-Improve-Website-Loading-and-Rendering-Speeds-feature.png" width="100%">
+            <div class="centered">
+              <h3 class="">For more information visit website</h3>
+              <small class="m-0">Dipadana, 20 January 2019</small>
+            </div>
           </div>
-        </b-jumbot>
-      </b-col>
+        </b-col>
+      </b-row>
 
-      <b-col cols="12" md="6" class="">
-        <div class="big-image p-0 m-1">
-          <img class="imageBig" src="https://content-static.upwork.com/blog/uploads/sites/3/2019/06/11041941/11-Tips-to-Optimize-JavaScript-And-Improve-Website-Loading-and-Rendering-Speeds-feature.png" width="100%">
-          <div class="centered">
-            <h3 class="">For more information visit website</h3>
-            <small class="m-0">Dipadana, 20 January 2019</small>
+      <b-row class="mt-4">
+        <b-col cols="12" md="8" >
+
+          <div class="border-bottom border-info mt-2 mb-4">
+            <div class=" card-body pb-1 pl-0">
+              <span class="m-0 bg-info p-2 text-white" style="font-size:18px">Article this day</span>
+            </div>
           </div>
-        </div>
-      </b-col>
-    </b-row>
 
-  <b-row class="mt-4">
-    <b-col cols="12" md="8" >
+          <b-card no-body class="overflow-hidden border-0 mb-4" style="">
+            <b-row no-gutters>
+              <b-col md="4" >
+                <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
+                <img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden">
+              </b-col>
+              <b-col md="8">
+                <b-card-body title="Horizontal Card">
+                  <p class="text-secondary mb-2"><small>Dipadana Putu, 20 January 2019</small></p>
+                  <b-card-text>
+                    This is a wider card with supporting text as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
 
-      <div class="border-bottom border-info mt-2 mb-4">
-        <div class=" card-body pb-1 pl-0">
-          <span class="m-0 bg-info p-2 text-white" style="font-size:18px">Article this day</span>
-        </div>
-      </div>
+          <b-card no-body class="overflow-hidden border-0 mb-4" style="">
+            <b-row no-gutters>
+              <b-col md="4" >
+                <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
+                <img src="https://picsum.photos/400/400/?image=22" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden">
+              </b-col>
+              <b-col md="8">
+                <b-card-body title="Horizontal Card">
+                  <p class="text-secondary mb-2"><small>Dipadana Putu, 20 January 2019</small></p>
+                  <b-card-text>
+                    This is a wider card with supporting text as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
 
-      <b-card no-body class="overflow-hidden border-0 mb-4" style="">
-        <b-row no-gutters>
-          <b-col md="4" >
-            <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
-            <img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden">
-          </b-col>
-          <b-col md="8">
-            <b-card-body title="Horizontal Card">
-              <p class="text-secondary mb-2"><small>Dipadana Putu, 20 January 2019</small></p>
-              <b-card-text>
-                This is a wider card with supporting text as a natural lead-in to additional content.
-                This content is a little bit longer.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
-      </b-card>
+          <b-card no-body class="overflow-hidden border-0 mb-4" style="">
+            <b-row no-gutters>
+              <b-col md="4" >
+                <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
+                <img src="https://picsum.photos/400/400/?image=19" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden">
+              </b-col>
+              <b-col md="8">
+                <b-card-body title="Horizontal Card">
+                  <p class="text-secondary mb-2"><small>Dipadana Putu, 20 January 2019</small></p>
+                  <b-card-text>
+                    This is a wider card with supporting text as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
 
-      <b-card no-body class="overflow-hidden border-0 mb-4" style="">
-        <b-row no-gutters>
-          <b-col md="4" >
-            <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
-            <img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden img-place">
-          </b-col>
-          <b-col md="8">
-            <b-card-body title="Horizontal Card">
-              <b-card-text>
-                This is a wider card with supporting text as a natural lead-in to additional content.
-                This content is a little bit longer.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
-      </b-card>
-    
-    
-    </b-col>
-  </b-row>
+          <b-card no-body class="overflow-hidden border-0 mb-4" style="">
+            <b-row no-gutters>
+              <b-col md="4" >
+                <!-- <b-card-img src="https://picsum.photos/400/400/?image=20" style="object-fit: cover;" class="rounded-0"></b-card-img> -->
+                <img src="https://picsum.photos/400/400/?image=21" style="object-fit: cover; height:20vh; width:100%;"  class="overflow-hidden">
+              </b-col>
+              <b-col md="8">
+                <b-card-body title="Horizontal Card">
+                  <p class="text-secondary mb-2"><small>Dipadana Putu, 20 January 2019</small></p>
+                  <b-card-text>
+                    This is a wider card with supporting text as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        
+        
+        </b-col>
 
-  
+        <b-col cols="0" md="4">
+          <b-list-group class="mt-5 pl-3">
+            <b-list-group-item class="d-flex justify-content-between align-items-center">
+              JavaScript
+              <b-badge variant="info" pill>14</b-badge>
+            </b-list-group-item>
 
-  </b-container>
+            <b-list-group-item class="d-flex justify-content-between align-items-center">
+              Basic Programming
+              <b-badge variant="info" pill>2</b-badge>
+            </b-list-group-item>
+
+            <b-list-group-item class="d-flex justify-content-between align-items-center">
+              PHP
+              <b-badge variant="info" pill>1</b-badge>
+            </b-list-group-item>
+          </b-list-group>
+        </b-col>
+
+      </b-row>
+
+      <b-row>
+        <b-col>
+
+        </b-col>
+      </b-row>
+
+    </b-container>
+
+    <b-container fluid class="p-0 mt-5">
+      <footer class="py-2 bg-info shadow">
+        <p class="text-center m-0 text-white">
+          <small>© 2019 MiniWordPress. All Rights Reserved</small>
+        </p>
+      </footer>
+    </b-container>
+  </div>
+
 </template>
 
 <script>
