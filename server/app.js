be65c46718bc3fb4ce207b53bfd9a46b9f1757be
ItsJoel88 <imanuelnjodi@gmail.com>
2019-11-10 18:46:30 +0700

    creating mongo atlas uri

--- server/app.js
+++ server/app.js
@@ -6,19 +6,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const cors = require('cors')
 const routes = require('./routes')
+const mongoose = require('./config/mongoose')
 const errorHandler = require('./middlewares/errorHandler')
 
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://new-user_31:screamaimfire@cluster0-96gdk.gcp.mongodb.net/miniwpproject?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-    useNewUrlParser: true
-});
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+mongoose()
 
 app.use(express.json())
 app.use(express.urlencoded({
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost:27017/vueminiwp', {
+    mongoose.connect("mongodb+srv://new-user_31:screamaimfire@cluster0-96gdk.gcp.mongodb.net/miniwpproject?retryWrites=true&w=majority", {
+            useCreateIndex: true,
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
+            useUnifiedTopology: true
         })
         .then(_ => {
-            console.log('connected successfully to mongoose database')
+            console.log('mongoose connected')
         })
         .catch(err => {
             console.log(err)
