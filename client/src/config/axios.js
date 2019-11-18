ab2912c312163b8ab999772eb3549daace3124a3
akbfitra <akbarfitralistyono@gmail.com>
2019-11-18 10:46:47 +0700

    deploy

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosServer = axios.create({
-    baseURL: `http://localhost:3000`,
+    baseURL: `http://34.87.6.238`,
     headers: {
       "accesstoken": localStorage.getItem('token')
     }
